581a95452c83ed2540eedc9818e2ef932c41a185
daff00 <daffakpc21@gmail.com>
2026-02-28 11:02:27 +0700

    fix: port on index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import mongo from './configurations/mongoConnection.js'
 import routes from './controllers/controller.js'
 
 const app = express();
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(express.urlencoded());
 app.use('/', routes);
 
-app.listen(port, () => {
-    console.log(`Example app is listening on ${port}`);
+app.listen(PORT, () => {
+    console.log(`Example app is listening on ${PORT}`);
 })
